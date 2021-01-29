717c3a83f044a841ced4326beb5a4f2e2639ded4
unknown <omaraakamal@gmail.com>
2021-01-29 10:56:39 -0600

    update 2.0

--- Exercise.sh
+++ Exercise.sh
@@ -20,9 +20,10 @@ fi
 if
 	cat /etc/group |grep Andrews_Group
 else
-	groupadd Andrews_Group -g 1020
-then
 	echo "Andrews_Group already exists"
+then
+	groupadd Andrews_Group -g 1020
+fi
 
 if  -b /dev/sdb1
 
@@ -31,24 +32,57 @@ then
 else
 	echo -e "n\np\n1\n\n3G\nw" | fdisk /dev/sdb
 	pvcreate /dev/sdb1
+fi
 
-if  -b /dev/sdc1
+if  [-b /dev/sdc1]
 
 then
 	echo "/dev/sdc1 exists"
 else
 	echo -e "n\np\n1\n\n3G\nw" | fdisk /dev/sdc
 	pvcreate /dev/sdc1
-
-if cat /etc/hostname |grep exercise_server
+fi
+if cat /etc/hostname == "exercise_server"
 then
 	echo "hostname already set up"
 else
-	vim /etc/hostname >> exercise_server
+	echo "exercise_server" >> /etc/hostname
+fi
 
 if cat /etc/hosts| grep 192.168.1.92 Server2
 then
 	echo "client alias already set"
 else
-	vim /etc/hosts >> 192.168.1.92 server2
+	 echo "192.168.1.92 server2" >> /etc/hosts 
+fi
+
+if vgs| grep exercisevg
+then
+	echo "VG is already created"
+else
+	echo vgcreate exercisevg /dev/sdb1 /dev/sdc1
+fi
+
+if lvs |grep exercise.fs
+then
+	echo "LV is already created"
+else
+	lvcreate -n exercise.fs -L 12G exercisevg
+fi
+
+if cat /etc/fstab| grep /dev/exercisevg/exercise.fs
+then
+	echo "xfs already in fstab, no action needed"
+else
+	 echo "/dev/exercisevg/exercise.fs /exercise xfs defaults 0 0" >> /etc/hosts 
+fi
+
+ifconfig enp0s3 192.168.1.99 netmask 192.168.1.99
+
+if cat /etc/selinux/config |grep SELINUX=disabled
+then
+	echo "selinux is already disabled"
+else
+	echo SELINUX=disabled >> /etc/selinux/config
+fi
 
