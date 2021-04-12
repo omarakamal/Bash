c972423a0c29b0307fbb084e1069af578b8dcf8d
unknown <omaraakamal@gmail.com>
2021-04-11 22:00:48 -0500

    first push

---
+++ Package_Installer.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "what package would you like to download?"
+read package
+if yum list installed |grep "$package"
+then
+        echo "this package is already installed in your Kernel"
+else
+        yum install "$package" -y
+fi
+~
